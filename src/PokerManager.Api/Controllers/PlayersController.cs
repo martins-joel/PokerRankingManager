faf869555472e5d6240f17d4f8a7578e8fe2a3f0
martins-joel <martins-joel@outlook.com>
2025-07-05 13:05:15 -0300

    Initial setup of tests and controller

---
+++ src/PokerManager.Api/Controllers/PlayersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokerManager.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlayersController : ControllerBase
+{
+    public PlayersController()
+    {
+    }
+
+    public async Task<IActionResult> GetPlayers()
+    {
+        return Ok("List of players");
+    }
+}
---
+++ test/PokerManager.Api.UnitTests/PlayersControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokerManager.Api.Controllers;
+
+namespace PokerManager.Api.UnitTests;
+
+public class PlayersControllerTests
+{
+    [Fact]
+    public async Task GetPlayers_OnSucess_ReturnsOk()
+    {
+        // Arrange
+        var controller = new PlayersController();
+
+        // Act
+        var result = await controller.GetPlayers() as OkObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(result.StatusCode, StatusCodes.Status200OK);
+    }
+}
