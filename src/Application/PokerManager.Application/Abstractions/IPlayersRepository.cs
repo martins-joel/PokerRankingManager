f3ffd34bbde2fe259723e9dc8bb52b14dcbf2340
martins-joel <martins-joel@outlook.com>
2025-07-08 00:42:40 -0300

    fix: Replacing wrongly created interface

--- src/Application/PokerManager.Application/Abstractions/IPlayersRepository.cs
+++
@@ -1,9 +0,0 @@
-public interface IPlayersRepository
-{
-    Task<List<Player>> GetPlayersAsync();
-    Task<Player?> GetPlayerByIdAsync(Guid id);
-    Task<Player?> GetPlayerByNameAsync(string name);
-    Task<Player> AddPlayerAsync(Player player);
-    Task<Player> UpdatePlayerAsync(Player player);
-    Task<bool> DeletePlayerAsync(Guid id);
-}
--- src/Core/PokerManager.Application/Abstractions/IPlayersRepository.cs
+++ src/Core/PokerManager.Application/Abstractions/IPlayersRepository.cs
@@ -1,8 +1,13 @@
-﻿using PokerManager.Domain.Entities;
+using PokerManager.Domain.Entities;
 
 namespace PokerManager.Application.Abstractions;
 
 public interface IPlayersRepository
 {
     Task<List<Player>> GetPlayersAsync();
+    Task<Player?> GetPlayerByIdAsync(Guid id);
+    Task<Player?> GetPlayerByNameAsync(string name);
+    Task<Player> AddPlayerAsync(Player player);
+    Task<Player> UpdatePlayerAsync(Player player);
+    Task<bool> DeletePlayerAsync(Guid id);
 }
