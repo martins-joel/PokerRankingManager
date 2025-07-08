f0adff37f1f1a9affdd85e4cc0f91935eaa2da1c
martins-joel <martins-joel@outlook.com>
2025-07-07 23:31:48 -0300

    feat: Added persistence services registration

---
+++ src/Infrastructure/PokerManager.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PokerManager.Application.Abstractions;
+using PokerManager.Persistence.Repositories;
+
+namespace PokerManager.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+        // Register persistence services
+        services.AddScoped<IPlayersRepository, PlayersRepository>();
+
+        return services;
+    }
+}
--- src/PokerManager.Api/Program.cs
+++ src/PokerManager.Api/Program.cs
@@ -1,4 +1,5 @@
 using PokerManager.Application;
+using PokerManager.Persistence;
 
 namespace PokerManager.Api;
 
@@ -10,6 +11,7 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddApplicationServices();
+        builder.Services.AddPersistenceServices();
         builder.Services.AddControllers();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
